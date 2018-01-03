c9af1437d6937502a61200eb644393ada24c1508
Enishowk <kenshineuh@gmail.com>
2018-01-03 22:09:24 +0100

    Add Form login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,39 +1,84 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Axios from "axios";
 
 export default class LoginPage extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      auth: false
+      login: "",
+      pass: ""
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  // login() {
-  //   const location = {
-  //     pathname: `/contacts`,
-  //     state: {}
-  //   };
-  //   setTimeout(
-  //     () => {
-  //       this.props.history.push(location);
-  //     },
-  //     1500
-  //   );
-  // }
+  handleInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    Axios.post("http://localhost:8000/login", {
+      username: this.state.login,
+      password: this.state.pass
+    })
+      .then(response => {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+
+        const location = {
+          pathname: `/contacts`,
+          state: {}
+        };
+        setTimeout(
+          () => {
+            this.props.history.push(location);
+          },
+          1500
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
         Login
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Login:
+            <input
+              name="login"
+              type="text"
+              value={this.state.login}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Pass:
+            <input
+              name="pass"
+              type="password"
+              value={this.state.pass}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <div className="nav-item">
-          {/* <button
-            onClick={() => {
-              this.login();
-            }}
-          >
-            Login
-          </button> */}
           <Link to="/contacts" className="button">
             <span className="icon">
               <i className="fa fa-address-book " />
@@ -45,3 +90,7 @@ export default class LoginPage extends React.Component {
     );
   }
 }
+
+LoginPage.propTypes = {
+  history: PropTypes.objectOf([PropTypes.function]).isRequired
+};
