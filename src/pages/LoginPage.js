14ab482873330cf386957dd8a5a3052600f0d052
Enishowk <kenshineuh@gmail.com>
2018-03-13 23:41:37 +0100

    feat: catch error on login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
 import Button from "material-ui/Button";
 import TextField from "material-ui/TextField";
+import Snackbar from "material-ui/Snackbar";
+import IconButton from "material-ui/IconButton";
+import CloseIcon from "material-ui-icons/Close";
 import ContactsServices from "../services/ContactsServices";
 
-export default class LoginPage extends React.Component {
+const styles = theme => ({
+  close: {
+    width: theme.spacing.unit * 4,
+    height: theme.spacing.unit * 4
+  }
+});
+
+class LoginPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
-      password: ""
+      password: "",
+      error: false
     };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClose = this.handleClose.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(event) {
@@ -44,11 +57,22 @@ export default class LoginPage extends React.Component {
       })
       .catch(error => {
         console.error(error);
+        this.setState({ error: true });
       });
   }
 
+  handleClose(event, reason) {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    this.setState({ error: false });
+  }
+
   render() {
-    const { username, password } = this.state;
+    const { username, password, error } = this.state;
+    const { classes } = this.props;
+
     return (
       <div>
         <div
@@ -73,6 +97,7 @@ export default class LoginPage extends React.Component {
               onChange={this.handleInputChange}
               type="text"
               value={username}
+              error={error}
             />
             <br />
             <TextField
@@ -81,6 +106,7 @@ export default class LoginPage extends React.Component {
               onChange={this.handleInputChange}
               type="password"
               value={password}
+              error={error}
             />
             <br />
             <br />
@@ -89,12 +115,37 @@ export default class LoginPage extends React.Component {
             </Button>
           </form>
         </div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left"
+          }}
+          open={error}
+          autoHideDuration={5000}
+          onClose={this.handleClose}
+          SnackbarContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          message={<span id="message-id">Login/Pass incorrect</span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={classes.close}
+              onClick={this.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </div>
     );
   }
 }
 
 LoginPage.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
   history: PropTypes.shape({
     length: PropTypes.number,
     action: PropTypes.string,
@@ -109,3 +160,5 @@ LoginPage.propTypes = {
     replace: PropTypes.func
   }).isRequired
 };
+
+export default withStyles(styles)(LoginPage);
